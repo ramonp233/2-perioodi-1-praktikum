4eecbaf111eb8afb631841e8f51a16a2b17f563c
Ramon <ramonp233@gmail.com>
2014-11-23 19:43:23 +0200

    Finish

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
 <head>
-    <title>2. Periood</title>
-    <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+    <title>2-periood</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 </head>
 <body>
- Tere
+    <?php $page_link = "second_page.php";
+    $name = "Ramon";
+    $lastname = "Pääsuke";
+    $age = 100; ?>
+    <a href="<?php echo $page_link.'?name='.$name."&lastname=".$lastname."&age=".$age; ?>">Teisele lehele</a>
+    <?php $example = array("name" => "Ramon", "lastname" => "Pääsuke"); ?><br>
+    <?php echo $_GET["test"]; ?>
 </body>
 </html>
--- second_page.php
+++ second_page.php
@@ -1,10 +1,15 @@
-<!DOCTYPE html>
+
+<!DOCTYPE HTML>
 <html>
 <head>
-    <title>2. Periood</title>
-    <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+    <title>2-periood</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link rel="stylesheet" type"text/css" href="style.css">
 </head>
 <body>
-
+    <a href="index.php?test=esimene leht">Esimesele lehele</a>
+    <br>
+    <?php echo "<p>".$_GET["name"]."</p>"; ?>
+    <?php $username=$_GET["name"]; ?>
 </body>
 </html>
